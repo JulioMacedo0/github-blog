d484dd2056f1c87d70a911cf3129e4975db9ef2c
Julio Macedo <juliocesar1076@gmail.com>
2022-10-20 11:09:53 -0300

    feat: create profile component

---
+++ src/Components/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+export const Profile = () => {
+  return (
+    <S.Container>
+      <S.ProfileContainer>
+        <img src="" alt="" />
+      </S.ProfileContainer>
+    </S.Container>
+  );
+};
---
+++ src/Components/Profile/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 32px 40px;
+`;
+
+export const ProfileContainer = styled.div`
+  img {
+    width: 148px;
+    height: 148px;
+  }
+`;
+
+export const ProfileInfo = styled.div``;
