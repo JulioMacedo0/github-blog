28a0a6512afbb038da60818d1b3034a463d4eebf
Julio Macedo <juliocesar1076@gmail.com>
2022-10-20 17:56:29 -0300

    UI: create Profile styles

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,10 +1,43 @@
 import * as S from "./styles";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowUpRightFromSquare,
+  faBuilding,
+  faUserGroup,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export const Profile = () => {
   return (
     <S.Container>
       <S.ProfileContainer>
-        <img src="" alt="" />
+        <img src="https://github.com/JulioMacedo0.png" alt="" />
+        <div>
+          <header>
+            <h2>Julio Macedo</h2>
+            <S.linkContainer>
+              <a href="">
+                Github <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+              </a>
+            </S.linkContainer>
+          </header>
+          <p>
+            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
+            volutpat pulvinar vel mass.
+          </p>
+
+          <S.Footer>
+            <span>
+              <FontAwesomeIcon icon={faGithub} /> cameronwll
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faBuilding} /> Rocketseat
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faUserGroup} /> 32 seguidores
+            </span>
+          </S.Footer>
+        </div>
       </S.ProfileContainer>
     </S.Container>
   );
--- src/Components/Profile/styles.ts
+++ src/Components/Profile/styles.ts
@@ -1,14 +1,108 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: absolute;
+  left: calc(50% - 864px / 2);
+  top: 208px;
+  width: 54rem;
+  height: 13.25rem;
   padding: 32px 40px;
+  border-radius: 8px;
+  background-color: ${(props) => props.theme["base-profile"]};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
 export const ProfileContainer = styled.div`
+  display: flex;
+  gap: 2rem;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+  header {
+    margin-top: 0.6rem;
+    display: flex;
+    justify-content: space-between;
+  }
   img {
-    width: 148px;
-    height: 148px;
+    border-radius: 8px;
+    width: 9.25rem;
+    height: 9.25rem;
+  }
+
+  h2 {
+    // font-family: "Nunito";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 1.5rem;
+    line-height: 130%;
+    color: ${(props) => props.theme["base-title"]};
+  }
+
+  p {
+    // font-family: "Nunito";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 160%;
+    color: ${(props) => props.theme["base-text"]};
   }
 `;
 
-export const ProfileInfo = styled.div``;
+export const linkContainer = styled.div`
+  align-self: flex-start;
+  border-bottom: 1px solid transparent;
+  transition: border-bottom 0.5s;
+
+  :hover {
+    border-bottom: 1px solid ${(props) => props.theme["blue"]};
+  }
+
+  a {
+    padding: 0px;
+    width: 4.1875rem;
+    height: 1.1875rem;
+    gap: 0.5rem;
+    display: flex;
+    justify-content: space-between;
+
+    font-style: normal;
+    font-weight: 700;
+    font-size: 0.75rem;
+    line-height: 160%;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: ${(props) => props.theme["blue"]};
+  }
+  svg {
+    align-self: center;
+    color: ${(props) => props.theme["blue"]};
+    width: 0.75rem;
+    height: 0.75rem;
+  }
+`;
+
+export const Footer = styled.footer`
+  margin-top: 1rem;
+  display: flex;
+  gap: 1.5rem;
+
+  span {
+    //  font-family: "Nunito";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 160%;
+    color: ${(props) => props.theme["base-text"]};
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+  svg {
+    color: ${(props) => props.theme["base-label"]};
+    width: 1.125rem;
+    height: 1.125rem;
+  }
+`;
