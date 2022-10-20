9c0dbda2351c9bca3c801c819ec8d3458b85190d
Julio Macedo <juliocesar1076@gmail.com>
2022-10-20 17:56:03 -0300

    UI: header styles

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -5,11 +5,12 @@ import rightEffect from "../../assets/right_effect.svg";
 export const Header = () => {
   return (
     <S.HeaderContainer>
-      <S.Effect src={leftEffect} height={188} />
       <S.Header>
-        <img src={logo} alt="logo" />
+        <S.LeftEffect src={leftEffect} height={188} />
+        <S.Logo src={logo} alt="logo" />
+        <S.BlurEffect />
+        <S.RightEffect src={rightEffect} height={236} />
       </S.Header>
-      <S.Effect src={rightEffect} height={236} />
     </S.HeaderContainer>
   );
 };
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,23 +1,45 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
-  background-color: ${(props) => props.theme["base-profile"]};
   width: 100%;
-  display: flex;
   max-height: 18.5rem;
 `;
 
 export const Header = styled.header`
+  background-color: ${(props) => props.theme["base-profile"]};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   width: 100%;
   height: 18.5rem;
   margin: 0 auto;
-  padding: 0 1rem;
   display: flex;
-  align-items: center;
-  justify-content: space-evenly;
+  align-items: start;
+  justify-content: space-between;
   max-width: 90rem;
 `;
 
-export const Effect = styled.img`
+export const BlurEffect = styled.div`
+  position: absolute;
+  width: 891px;
+  height: 52px;
+  left: calc(50% - 891px / 2 - 2.5px);
+  top: 235px;
+  background: #14589c;
+  filter: blur(106px);
+`;
+
+export const LeftEffect = styled.img`
+  margin-top: 4.375rem;
   height: auto;
+`;
+
+export const RightEffect = styled.img`
+  margin-top: 1.875rem;
+  margin-right: 0.125rem;
+  height: auto;
+`;
+
+export const Logo = styled.img`
+  position: relative;
+  left: -18px;
+  margin-top: 4rem;
 `;
