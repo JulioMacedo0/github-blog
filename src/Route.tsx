95a727b9c1ee440a931e46be575bfc9faf466800
Julio Macedo <juliocesar1076@gmail.com>
2022-10-21 17:48:11 -0300

    feat: add new route

--- src/Route.tsx
+++ src/Route.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 
 import { Home } from "./pages/Home";
+import { Posts } from "./pages/Post";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
       </Route>
     </Routes>
   );
