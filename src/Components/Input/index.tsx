f053a45378d35526574e11940cff4dd12b1cb02c
Julio Macedo <juliocesar1076@gmail.com>
2022-10-21 14:32:41 -0300

    feat: use post context

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
+import { PostsContext } from "../../context/PostsContext";
 import * as S from "./styles";
 
 export const Input = () => {
+  const { post } = useContext(PostsContext);
   return (
     <S.HeaderContainer>
       <div>
-        <p>Publicações</p> <span>6 publicações</span>
+        <p>Publicações</p> <span>{post.length} publicações</span>
       </div>
       <input type="text" placeholder="Buscar conteúdo" />
     </S.HeaderContainer>
