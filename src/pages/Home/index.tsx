a94a14b967636b5d76c85ea9c55e97acb6038af0
Julio Macedo <juliocesar1076@gmail.com>
2022-10-21 11:53:12 -0300

    feat: I did a lot of things

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from "react";
+import { api } from "../../API/api";
+import { CardPost } from "../../Components/CardPost";
+import { Input } from "../../Components/Input";
 import { Profile } from "../../Components/Profile/Profile";
 import * as S from "./styles";
+
+interface PostInterface {
+  id: number;
+  url: string;
+  title: string;
+  body: string;
+  created_at: string;
+  comments: number;
+}
+
 export const Home = () => {
+  const [post, setPost] = useState<PostInterface[]>([]);
+
+  const getPosts = async () => {
+    const response = await api.get("/repos/juliomacedo0/github-blog/issues");
+    setPost(response.data);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <S.HomeContainer>
-      <Profile />;
+      <S.Container>
+        <S.Main>
+          <Profile />
+          <Input />
+
+          <S.Grid>
+            {post.map((post) => {
+              return (
+                <CardPost title={post.title} body={post.body} key={post.id} />
+              );
+            })}
+          </S.Grid>
+        </S.Main>
+      </S.Container>
     </S.HomeContainer>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,3 +5,24 @@ export const HomeContainer = styled.div`
   display: flex;
   justify-content: center;
 `;
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 90rem;
+
+  background-color: ${(props) => props.theme["base-background"]};
+`;
+
+export const Main = styled.main`
+  width: 100%;
+  max-width: 54rem;
+`;
+
+export const Grid = styled.div`
+  margin-bottom: 14.625rem;
+  padding: 0px 1rem;
+  margin-top: 3rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2rem;
+`;
