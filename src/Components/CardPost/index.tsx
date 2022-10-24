a8c8e5e2b06e7058ca0a4c2d007bb27339628c79
Julio Macedo <juliocesar1076@gmail.com>
2022-10-24 14:51:22 -0300

    feat: pass function in props

--- src/Components/CardPost/index.tsx
+++ src/Components/CardPost/index.tsx
@@ -1,14 +1,18 @@
 import * as S from "./styles";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { PostsContext } from "../../context/PostsContext";
 interface CardPostProps {
   title: string;
   body: string;
+  postId: number;
 }
 
-export const CardPost = ({ title, body }: CardPostProps) => {
+export const CardPost = ({ title, body, postId }: CardPostProps) => {
+  const { haddleChangeCurrentPostId } = useContext(PostsContext);
   return (
     <S.Wrapper>
-      <Link to="/posts">
+      <Link to="/posts" onClick={() => haddleChangeCurrentPostId(postId)}>
         <S.Card>
           <div>
             <h3>{title} </h3>
