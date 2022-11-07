dfae4d4d749c3aa3480da825e9b5e18c80b2c093
Julio Macedo <juliocesar1076@gmail.com>
2022-11-07 10:36:24 -0300

    feat: add onChange in input

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -3,13 +3,18 @@ import { PostsContext } from "../../context/PostsContext";
 import * as S from "./styles";
 
 export const Input = () => {
-  const { post } = useContext(PostsContext);
+  const { post, onChangeSetInput, inputValue } = useContext(PostsContext);
   return (
     <S.HeaderContainer>
       <div>
         <p>Publicações</p> <span>{post.length} publicações</span>
       </div>
-      <input type="text" placeholder="Buscar conteúdo" />
+      <input
+        type="text"
+        placeholder="Buscar conteúdo"
+        value={inputValue}
+        onChange={(event) => onChangeSetInput(event.target.value)}
+      />
     </S.HeaderContainer>
   );
 };
