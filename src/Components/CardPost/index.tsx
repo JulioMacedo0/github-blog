72ca4f35b11ec7ca7f3be1c4c8d74d1fd2022875
Julio Macedo <juliocesar1076@gmail.com>
2022-11-07 08:29:38 -0300

    feat: add distance between dates

--- src/Components/CardPost/index.tsx
+++ src/Components/CardPost/index.tsx
@@ -2,21 +2,29 @@ import * as S from "./styles";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { PostsContext } from "../../context/PostsContext";
+import { formatDistance } from "date-fns";
+import { ptBR } from "date-fns/locale";
 interface CardPostProps {
   title: string;
   body: string;
   postId: number;
+  date: string;
 }
 
-export const CardPost = ({ title, body, postId }: CardPostProps) => {
+export const CardPost = ({ title, body, postId, date }: CardPostProps) => {
   const { haddleChangeCurrentPostId } = useContext(PostsContext);
+
+  const distanceBetweenDates = formatDistance(Date.parse(date), new Date(), {
+    locale: ptBR,
+  });
+
   return (
     <S.Wrapper>
       <Link to="/posts" onClick={() => haddleChangeCurrentPostId(postId)}>
         <S.Card>
           <div>
             <h3>{title} </h3>
-            <span>Há 1 dia</span>
+            <span>{distanceBetweenDates}</span>
           </div>
 
           <p>{body}</p>
