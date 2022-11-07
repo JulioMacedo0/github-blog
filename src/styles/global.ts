01bce55c90e887ddf0de8434634826b077d5a109
Julio Macedo <juliocesar1076@gmail.com>
2022-11-07 15:25:10 -0300

    feat: style in scroll bar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,7 +7,30 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
 }
 
+::-webkit-scrollbar {
+    width: 10px;
+    height: 10px
+}
+::-webkit-scrollbar-track {
+ 
 
+}
+
+
+::-webkit-scrollbar-thumb {
+  background: rgba(255,255,255,0.20);
+ 
+
+}
+pre {
+    margin: 1rem;
+    border-radius: 8px;
+    border: solid 1px #fff;
+}
+
+code {
+    border-radius: 8px;
+}
 body {
     background-color: ${(props) => props.theme["base-input"]};
     -webkit-font-smoothing: antialiased;
